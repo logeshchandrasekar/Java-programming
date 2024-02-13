ae1cc5502ec2b2e669917a58b2f8f153529564af
logeshchandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2024-02-13 09:24:12 +0530

    Add files via upload

---
+++ DAY 4/Arithmeticexception.java
@@ -0,0 +1,15 @@
+public class Arithmeticexception {
+    public static void main(String[] args){
+        try
+        {
+
+            int a=10;
+            int b=0;
+            int c=a/b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println("there is an exception"+e);
+        }
+    }
+}
