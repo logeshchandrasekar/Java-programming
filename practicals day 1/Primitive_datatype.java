9ac79a6fbbbe79ad17f386425295859dbb517326
logeshchandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2024-03-19 08:27:53 +0530

    Add files via upload

---
+++ practicals day 1/Primitive_datatype.java
@@ -0,0 +1,21 @@
+public class Primitive_datatype
+{
+        public static void main(String[] args) {
+            byte byteVar = 20;
+            short shortVar = 150;
+            int intVar = 500;
+            long longVar = 100000L;
+            float floatVar = 13.5f;
+            double doubleVar = 167.1234;
+            char charVar = 'P';
+            boolean boolVar = true;
+            System.out.println("byteVar: " + byteVar);
+            System.out.println("shortVar: " + shortVar);
+            System.out.println("intVar: " + intVar);
+            System.out.println("longVar: " + longVar);
+            System.out.println("floatVar: " + floatVar);
+            System.out.println("doubleVar: " + doubleVar);
+            System.out.println("charVar: " + charVar);
+            System.out.println("boolVar: " + boolVar);
+        }
+    }
